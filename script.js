60f39fe9da4e6a32acb86a87ddc8e13c367466fd
H-Abdullah <abdullah.ebn.hasan@gmail.com>
2023-07-16 11:41:24 +0800

    - Add 3-related-files all in one. - Add simple button and popup-menu for entering book details - Not yet adding js function to add detail into pages(because im lazy and i know(probably) how to do it.

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggleForm = document.getElementById("overlay");
+
+function toggle() {
+  toggleForm.classList.toggle("active");
+}
+
+let myLibrary = [];
+
+function Book() {
+
+}
+
+function addBookToLibrary() {
+
+}
+
